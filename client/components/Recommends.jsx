c4416a6b8e851d953da6cbda09ecb650843b7fc4
peter <peterliu1991@gmail.com>
2019-02-07 11:32:42 -0800

    getRecommends function for getting track list

--- client/components/Recommends.jsx
+++ client/components/Recommends.jsx
@@ -1,6 +1,17 @@
 class Recommends extends React.Component {
   constructor() {
     super();
+    this.state = {
+      recommends: []
+    }
+  }
+
+  getRecommends() {
+    axios.get('http://127.0.0.1:3003/recommends')
+      .then(res => {
+        this.setState({ recommends: res });
+      })
+      .catch(err => { console.log(err) });
   }
 
   render() {
