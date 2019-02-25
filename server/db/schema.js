9d87374b3142140aa7575ff2306ca42e7540a7e3
peter <peterliu1991@gmail.com>
2019-02-24 17:21:36 -0800

    fixed db creation and seeding

--- server/db/schema.js
+++ server/db/schema.js
@@ -1,15 +1,32 @@
 const mdb = require('./db.js');
 
-mdb.query('CREATE DATABASE IF NOT EXISTS fec2', err => {
-  if (err) { console.log(err); }
-  mdb.query('USE fec2', err => {
-    if (err) { console.log(err); }
-    mdb.query('DROP TABLE IF EXISTS tracks', err => {
-      if (err) { console.log(err); }
-      mdb.query('CREATE TABLE tracks (_id INTEGER NOT NULL AUTO_INCREMENT, track_id VARCHAR(255) NOT NULL UNIQUE, title VARCHAR(255) NOT NULL, artist VARCHAR(255), album VARCHAR(255), PRIMARY KEY (_id))', err => {
-        if (err) { console.log(err); }
+mdb.query('CREATE DATABASE IF NOT EXISTS hrr36_fec2', err => {
+  if (err) {
+    console.log(err);
+    mdb.end();
+  } else {
+    mdb.query('USE hrr36_fec2', err => {
+      if (err) {
+        console.log(err);
         mdb.end();
-      });
+      } else {
+        mdb.query('DROP TABLE IF EXISTS tracks', err => {
+          if (err) {
+            console.log(err);
+            mdb.end();
+          } else {
+            mdb.query('CREATE TABLE tracks (_id INTEGER NOT NULL AUTO_INCREMENT, track_id VARCHAR(255) NOT NULL UNIQUE, title VARCHAR(255) NOT NULL, artist VARCHAR(255), album VARCHAR(255), PRIMARY KEY (_id))', err => {
+              if (err) {
+                console.log(err);
+                mdb.end();
+              } else {
+                console.log('Database initialized.');
+                mdb.end();
+              }
+            });
+          }
+        });
+      }
     });
-  });
+  }
 });
--- server/db/seed.js
+++ server/db/seed.js
@@ -5,6 +5,8 @@ const mdb = require('./db.js');
 fs.readFile(path.join(__dirname, 'featured.json'), 'utf8', (err, data) => {
   if (err) {
     console.log(err);
+    mdb.end();
+    process.exit();
   } else {
     let json = JSON.parse(data).aTracks;
     let tracks = [];
@@ -21,12 +23,23 @@ fs.readFile(path.join(__dirname, 'featured.json'), 'utf8', (err, data) => {
       tracks.push([track.track_id, track.track_title, track.artist_name, track.album_title]);
     }
 
-    mdb.query('INSERT INTO tracks (track_id, title, artist, album) VALUES ?', [tracks], (err, res) => {
+    mdb.query('USE hrr36_fec2', err => {
       if (err) {
         console.log(err);
-      } else {
         mdb.end();
         process.exit();
+      } else {
+        mdb.query('INSERT INTO tracks (track_id, title, artist, album) VALUES ?', [tracks], err => {
+          if (err) {
+            console.log(err);
+            mdb.end();
+            process.exit();
+          } else {
+            console.log('Database seeded.');
+            mdb.end();
+            process.exit();
+          }
+        });
       }
     });
   }
